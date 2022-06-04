666076d3c092ffaaa5c341f09a633bf262506003
hamde abo khadra <hamdeahmed01221@gmail.com>
2022-06-04 04:39:26 +0200

    create database

---
+++ database/migrations/2022_06_04_023213_create_portfolios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->string('tags');
+            $table->unsignedBigInteger('portfolio_category_id');
+            $table->foreign('portfolio_category_id')->references('id')->on('portfolio__categories')->onDelete('CASCADE');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
